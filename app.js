d5322b973e85d3fa409673de60682f207db1b5dc
Laura <lausomoreno99@gmail.com>
2020-03-01 14:51:59 -0600

    Moved the js file into the script section of the html folder

--- app.js
+++ app.js
@@ -1,28 +1,15 @@
 
-//el double equals en java es triple equals en script
-// let para declarar cualquier variable, y a diferencia
-// de java no se necesita poner el tipo adelante
-// switch, case and break 
-//metodos is function
-
-// en clase student podria tener 
 let students = [];
 let courses =[];
 
+
 console.log("test!!!!");
-console.log(students);
-setTimeout(()=>{
-    let result1 =document.getElementById("ButtonS");
-    console.log(result1);
-    if(result1){
-        console.log("adding event listener for buttonC")
-        result1.addEventListener("click",addStudent); 
-    }
-},2000)
+console.log(document.getElementById("ButtonS"));
 
+  
 
-
-function addStudent(){
+const addStudent = (ev) =>{
+    ev.preventDefault();
   
     let student = {
     
@@ -35,13 +22,15 @@ function addStudent(){
     students.push(student);
     document.forms[0].reset;
     console.log("Reached here");
+    console.log(students);
 }
+document.addEventListener('DOMContentLoaded',()=>{
+document.getElementById("ButtonS").addEventListener("click", addStudent);
+});
 
-
-   
-
-   function  addCourse(){
-    let course = {
+   const  addCourse =  (ev) =>{
+    ev.preventDefault();
+    let course =  {
    
         
         nameC: document.getElementById("namec").value,
@@ -56,12 +45,12 @@ document.forms[0].reset;
 alert("The course has been added succesfully");
 
     }
+    document.addEventListener('DOMContentLoaded',()=>{
+        document.getElementById("ButtonC").addEventListener("click", addCourse);
+        });
 
 
-    var result =document.getElementById("ButtonC");
-    if(result){
-        ButtonC.addEventListener("click",addCourse);
-    }
+   
 
     function deleteStudent(student){
 
